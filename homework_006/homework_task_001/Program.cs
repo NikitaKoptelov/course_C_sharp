6d524808b158df81c72f928e80fd51e31037d19c
NikitaKoptelov <nikita.koptelov2018@yandex.ru>
2022-08-22 12:59:53 +0800

    решение ДЗ № 6

---
+++ homework_006/homework_task_001/Program.cs
@@ -0,0 +1,104 @@
+﻿//       Задача:
+// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+//         Программа :
+
+
+string GetOneArray (string nub, int index)
+{
+    
+    string[] twoArray = nub.Split(",");
+    return twoArray[index];
+}
+
+string GetTwoArray (string nub, int index1, int index2)
+{
+    
+    string[,] twoArray = new string[2, 5];
+    int k = 0;
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            twoArray[i,j] = GetOneArray(nub, k);
+            k++;
+        }
+        k = 5;
+    }
+    return twoArray[index1, index2];
+}
+
+int GetSumMax (string nub)
+{
+    
+    int sumNub = 0;
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if ((Convert.ToInt32(GetTwoArray(nub, i, j))) > 0)
+            {
+                
+                sumNub +=1;
+            }
+        }
+    }
+    return sumNub;
+}
+
+int GetSumMaxS1 (string nub)
+{
+    int sumNubS1 = 0;
+    for (int j = 0; j < 5; j++)
+    {
+        if ((Convert.ToInt32(GetTwoArray(nub, 0, j))) > 0)
+        {
+            sumNubS1 += 1;
+        }
+    }
+    return sumNubS1;
+}
+
+int GetSumMaxS2 (string nub)
+{
+    int sumNubS2 = 0;
+    for (int j = 0; j < 5; j++)
+    {
+        if ((Convert.ToInt32(GetTwoArray(nub, 1, j))) > 0)
+        {
+            sumNubS2 += 1;
+        }
+    }
+    return sumNubS2;
+}
+
+
+
+
+
+
+Console.Write("введите 10 числа через знак (,) - ");
+string nub = Console.ReadLine();
+
+PrintTweArray();
+Console.Write("колличество чисел >0 общее - ");
+Console.WriteLine(GetSumMax(nub));
+Console.Write("колличество чисел >0 строки1 - ");
+Console.WriteLine(GetSumMaxS1(nub));
+Console.Write("колличество чисел >0 строки2 - ");
+Console.WriteLine(GetSumMaxS2(nub));
+
+void PrintTweArray ()
+{
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            Console.Write($"{GetTwoArray(nub, i, j)} ");
+        }
+        Console.Write("\n");
+    }
+}
+
---
+++ homework_006/homework_task_002/Program.cs
@@ -0,0 +1,38 @@
+﻿//           Задача:
+// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//                     k1 * x + b1 = k2 * x + b2
+//                             b1 = k2 * x + b2 - k1 * x
+//                        b1 - b2 = (k2 - k1) * x
+//               (b1 - b2) / (k2 - k1) = x
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+//         Программа:
+
+
+Console.Write("введите число k1 - ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("введите число b1 - ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("введите число k2 - ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("введите число b2 - ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("координата точки пересечения - ");
+Console.WriteLine($"({GetXNub(k1, b1)}; {GetYNub(k2, b2)})");
+
+
+
+double GetXNub (double k1, double b1)
+{
+    double x = (b1 - b2) / (k2 - k1);
+    return x;
+}
+
+double GetYNub (double k2, double b2)
+{
+    double y = k2 * GetXNub(k1, b1) + b2;
+    return y;
+}
+
+
